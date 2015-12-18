7777219551b9cd2da3518b18e65fa55402c6da8d
MK Liew <mk.liew@datapel.com>
2015-12-18 16:47:04 +0800

    Adding return object for ReplicateSQL Endpoint.

---
+++ Datapel.BreezeAPI.Client.Domain/Contract/RTQ.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Datapel.BreezeAPI.SDK.Contract
+{
+    public class RTQ : WMSBaseEntity
+    {
+        public int RTQ_ID { get; set; }
+        public long RTQ_SyncTime { get; set; }
+        public long RTQ_SyncDate { get; set; }
+        public int RTQ_IsSynced { get; set; }
+        public string RTQ_MsgSQL { get; set; }
+        public int RTQ_MsgStatus { get; set; }
+        public string RTQ_Msg { get; set; }
+        public string RTQ_ErrorMsg { get; set; }
+    }
+}
