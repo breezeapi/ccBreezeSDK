5f764a5ef24f8f5758fac36fa869ef31caa3d811
MK Liew <mk.liew@datapel.com>
2016-05-31 16:11:54 +0800

    added DBSchemaService which will perform: 1. Get the DB Schema version of the database that Breeze DataServer Connected. 2. Perform the upgrade of the  db to a given version by calling the DBSchema endpoint.

---
+++ Datapel.BreezeAPI.Client.Domain/Service/DBSchemaService.cs
@@ -0,0 +1,43 @@
+﻿using Datapel.BreezeAPI.SDK.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Datapel.BreezeAPI.SDK.Service
+{
+    public class DBSchemaService : BreezeServiceBase, IDisposable
+    {
+        public DBSchemaService()
+        {
+            base.endpoint = "DBSchema";
+        }
+
+        public int GetVersion()
+        {
+            var query = FILTER_STR_HEADER + "*";
+            var path = GenerateGetPath(HttpMethods.GET, query);
+            UseCompression = true;
+            var json = WebClient.Get(path).ToString();
+            int ret = 0; 
+            if (! int.TryParse(json, out ret))
+            {
+
+            }
+            return ret; 
+        }
+        public int UpgradeDBSchema(int schemaVersion)
+        {
+            var query = FILTER_STR_HEADER + "*" + "&version=" + schemaVersion.ToString();
+            var path = GenerateGetPath(HttpMethods.GET, query);
+            UseCompression = true;
+            var json = WebClient.Get(path).ToString();
+            int ret = 0;
+            if (! int.TryParse(json, out ret))
+            {
+
+            }
+            return ret;
+        }
+    }
+}
